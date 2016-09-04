a0d1e75f73cafa35a44a77c469cef5a976d1a340
M&R Games <mrgames@outlook.de>
2016-09-04 19:26:10 +0200

    (c) M&R Games 2016

---
+++ app/src/main/java/com/mrgames13/jimdo/bsbz_app/App/ImagePickerActivity.java
@@ -0,0 +1,23 @@
+package com.mrgames13.jimdo.bsbz_app.App;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mrgames13.jimdo.bsbz_app.R;
+
+public class ImagePickerActivity extends AppCompatActivity {
+
+    //Konstanten
+
+    //Variablen als Objekte
+
+    //Variablen
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_picker);
+    }
+
+
+}
