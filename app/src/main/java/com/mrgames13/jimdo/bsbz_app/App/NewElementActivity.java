a918884c68f7c7b00e8f9c595d1d9257c266e1a3
M&R Games <mrgames@outlook.de>
2016-09-13 15:40:48 +0200

    (c) M&R Games 2016

---
+++ app/src/main/java/com/mrgames13/jimdo/bsbz_app/App/NewElementActivity.java
@@ -0,0 +1,23 @@
+package com.mrgames13.jimdo.bsbz_app.App;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.mrgames13.jimdo.bsbz_app.R;
+
+public class NewElementActivity extends AppCompatActivity {
+    //Konstanten
+
+    //Variablen als Objekte
+
+    //Variablen
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_element);
+    }
+
+
+}
