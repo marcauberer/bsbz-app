00b1fe86f6465a30f7c895e7542b120cff7f4e32
M&R Games <mrgames@outlook.de>
2016-09-22 18:33:22 +0200

    (c) M&R Games 2016

--- app/src/main/java/com/mrgames13/jimdo/bsbz_app/RecyclerViewAdapters/NewsViewAdapter.java
+++ app/src/main/java/com/mrgames13/jimdo/bsbz_app/RecyclerViewAdapters/NewsViewAdapter.java
@@ -9,6 +9,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.mrgames13.jimdo.bsbz_app.App.MainActivity;
+import com.mrgames13.jimdo.bsbz_app.ComponentClasses.New;
 import com.mrgames13.jimdo.bsbz_app.R;
 
 public class NewsViewAdapter extends RecyclerView.Adapter<NewsViewAdapter.ViewHolderClass> {
@@ -23,15 +24,21 @@ public class NewsViewAdapter extends RecyclerView.Adapter<NewsViewAdapter.ViewHo
         //Variablen als Objekte
         private TextView item_icon;
         private TextView item_subject;
+        private TextView item_description;
+        private TextView item_receiver;
+        private TextView item_writer;
         private TextView item_date;
         private ImageView item_dropdown_arrow;
 
         public ViewHolderClass(View itemView) {
             super(itemView);
             item_icon = (TextView) itemView.findViewById(R.id.item_icon);
-            item_subject = (TextView) itemView.findViewById(R.id.item_subject);
-            item_date = (TextView) itemView.findViewById(R.id.item_date);
             item_dropdown_arrow = (ImageView) itemView.findViewById(R.id.item_dropdown_arrow);
+            item_subject = (TextView) itemView.findViewById(R.id.item_subject);
+            item_description = (TextView) itemView.findViewById(R.id.item_description);
+            item_date = (TextView) itemView.findViewById(R.id.item_date);
+            item_receiver = (TextView) itemView.findViewById(R.id.item_receiver);
+            item_writer = (TextView) itemView.findViewById(R.id.item_writer);
         }
     }
 
@@ -44,6 +51,13 @@ public class NewsViewAdapter extends RecyclerView.Adapter<NewsViewAdapter.ViewHo
 
     @Override
     public void onBindViewHolder(final ViewHolderClass holder, final int pos) {
+        //Daten befüllen
+        New n = MainActivity.news.get(pos);
+        holder.item_subject.setText(n.getSubject());
+        holder.item_description.setText(n.getDescription());
+        holder.item_date.setText(n.getActivationDate());
+        holder.item_receiver.setText(n.getReceiver());
+        holder.item_writer.setText(n.getWriter());
 
         //OnClickListener setzen
         holder.itemView.setOnClickListener(new View.OnClickListener() {
