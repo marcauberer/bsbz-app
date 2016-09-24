ea198f74c172044dfe2b3d799d8eff722f14d358
M&R Games <mrgames@outlook.de>
2016-09-24 13:00:03 +0200

    (c) M&R Games 2016

--- app/src/main/java/com/mrgames13/jimdo/bsbz_app/RecyclerViewAdapters/NewsViewAdapter.java
+++ app/src/main/java/com/mrgames13/jimdo/bsbz_app/RecyclerViewAdapters/NewsViewAdapter.java
@@ -5,6 +5,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -70,6 +72,9 @@ public class NewsViewAdapter extends RecyclerView.Adapter<NewsViewAdapter.ViewHo
                     //Collapse item
                     holder.itemView.findViewById(R.id.item_area_description).setVisibility(View.GONE);
                     holder.itemView.findViewById(R.id.item_button_container).setVisibility(View.GONE);
+                    //Rotations-Animation für DropDown-Arrow starten
+                    Animation rot = AnimationUtils.loadAnimation(holder.itemView.getContext(), R.anim.item_element_rotation);
+
                 } else {
                     //Expand item
                     holder.itemView.findViewById(R.id.item_area_description).setVisibility(View.VISIBLE);
